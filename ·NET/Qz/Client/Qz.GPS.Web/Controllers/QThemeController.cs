781b9826f55f3199259f5ba8ec2966d3f3f63f31
Qlinzpc <qilinzpc@163.com>
2015-07-14 11:53:59 +0800

    新增 3种主题

---
+++ ·NET/Qz/Client/Qz.GPS.Web/Controllers/QThemeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Qz.GPS.Web.Controllers
+{
+    public class QThemeController : Controller
+    {
+        //
+        // GET: /Theme/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Win7()
+        {
+            return View();
+        }
+
+        public ActionResult Win8()
+        {
+            return View();
+        }
+
+    }
+}
